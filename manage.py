6afebda6c689ed869a5c4d3f1d0c8a4276354cb2
coding-happily <liuqingbin100@gmail.com>
2016-08-23 23:51:41 +0800

    add migrate

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 from flask.ext.script import Manager, Server
-from main import app, db, User, Post, Comment
+from flask.ext.migrate import Migrate, MigrateCommand
 
-manager = Manager(app)
+from main import app, db, User, Post, Comment, Tag
+
+manager = Manager(app,db)
 
 manager.add_command('server', Server())
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post, Comment=Comment)
+    return dict(app=app, db=db, User=User, Post=Post, Comment=Comment, Tag=Tag)
 
 if __name__ == "__main__":
     manager.run()
