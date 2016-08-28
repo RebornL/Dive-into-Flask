0f0663f52657a4e08b328a2170529e857a0075f1
coding-happily <liuqingbin100@gmail.com>
2016-08-28 21:24:21 +0800

    add the WTF

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 class Config(object):
-    pass
+    SECRET_KEY='asdhjhjhaqlasdhh1231'
 
 class ProdConfig(object):
     pass
 
 class DevConfig(object):
     DEBUG = True
+    SECRET_KEY='asdhjhjhaqlasdhh1231'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 from config import DevConfig
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import func
+from wtforms import StringField, TextAreaField
+from flask_wtf import Form
+from wtforms.validators import DataRequired, Length
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -78,6 +82,11 @@ class Tag(db.Model):
     def __repr__(self):
         return "<Tag '{}'>".format(self.title) 
 
+#评论表单
+class CommentForm(Form):
+    name = StringField('Name', validators=[DataRequired(), Length(max=255)])
+    text = TextAreaField(u'Comment', validators=[DataRequired()])
+
 #侧边栏信息
 def sidebar_data():
     recent = Post.query.order_by(Post.publish_date.desc()).limit(5).all()
@@ -98,14 +107,24 @@ def home(page=1):
 
     return render_template('home.html',posts=posts, recent=recent, top_tags=top_tags)
 
-@app.route('/post/<int:post_id>')
+@app.route('/post/<int:post_id>',methods=('GET', 'POST'))
 def post(post_id):
     post = Post.query.get_or_404(post_id)#根据post_id获取post
     tags = post.tags#通过tags获取标签
     comments = post.comments.order_by(Comment.date.desc()).all()
     recent,top_tags = sidebar_data()
+    #新建一个评论框
+    form = CommentForm()
+    if form.validate_on_submit():
+        new_comment = Comment()
+        new_comment.name = form.name.data
+        new_comment.text = form.text.data
+        new_comment.post_id = post_id
+        new_comment.date = datetime.datetime.now()
+        db.session.add(new_comment)
+        db.session.commit()
 
-    return render_template('post.html',post=post, tags=tags, comments=comments, recent=recent, top_tags=top_tags)
+    return render_template('post.html',post=post, tags=tags, comments=comments, recent=recent, top_tags=top_tags,form=form)
 
 @app.route('/tag/<string:tag_name>')
 def tag(tag_name):
